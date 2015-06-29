c81114e4878e600b346bb4555cce6a1bc62bfc60
Jonathan Marmor <jmarmor@gmail.com>
2015-06-29 15:34:13 -0600

    Move some things that might be general purpose from animal_play_harmony to utils.

--- shortstories/utils.py
+++ shortstories/utils.py
@@ -146,3 +146,52 @@ def get_intervals(harmony):
             if top in harmony:
                 intervals[interval].append((bottom, top))
     return dict(intervals)
+
+
+def build_chord_type_on_root(root, chord_type):
+    """
+    >>> build_chord_type_on_root(9, [4, 3, 5])
+    [9, 1, 4]
+
+    """
+    chord = [root]
+    for interval in chord_type[:-1]:
+        chord.append((chord[-1] + interval) % 12)
+    return chord
+
+
+def build_chord_type_on_all_roots(chord_type):
+    """
+    >>> build_chord_type_on_all_roots([4, 3, 5])
+    [[0, 4, 7],
+     [1, 5, 8],
+     [2, 6, 9],
+     [3, 7, 10],
+     [4, 8, 11],
+     [5, 9, 0],
+     [6, 10, 1],
+     [7, 11, 2],
+     [8, 0, 3],
+     [9, 1, 4],
+     [10, 2, 5],
+     [11, 3, 6]]
+
+    """
+    return [build_chord_type_on_root(root, chord_type) for root in range(12)]
+
+
+def pitches_to_intervals(chord):
+    """
+    >>> pitches_to_intervals([12, 16, 31])
+    [4, 3, 5]
+    """
+    # TODO rewrite this
+    pcs = sorted(list(set([p % 12 for p in chord])))
+    pcs = [p - pcs[0] for p in pcs]
+    pcs.reverse()
+    pcs.insert(0, 12)
+    intervals = []
+    for a, b in pairwise(pcs):
+        intervals.append(a - b)
+    intervals.reverse()
+    return intervals
