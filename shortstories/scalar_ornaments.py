fd68975d0d3f3bfa9becb51f8731b22f6433d675
Jonathan Marmor <jmarmor@gmail.com>
2015-06-29 22:31:31 -0400

    Clean up scalar_ornaments.py.

--- shortstories/scalar_ornaments.py
+++ shortstories/scalar_ornaments.py
@@ -6,44 +6,53 @@ from collections import defaultdict
 from utils import pairwise
 
 
-def is_good(ornament):
-    # Get rid of repeated notes
-    if ornament[-1] == 0:
-        return False
+def is_good(ornament, biggest_interval=3):
+    # Put the target note at the end of the ornament
+    # to prevent repeated notes or large intervals
+    # between the last note of the ornament and the target note
+    ornament = list(ornament)
+    ornament.append(0)
 
     pairs = pairwise(ornament)
 
     for a, b in pairs:
+        # Don't allow repeated pitches
         if a == b:
             return False
 
-        if abs(a - b) > 3:
+        # Don't allow transitions greater than `biggest_interval` steps
+        if abs(a - b) > biggest_interval:
             return False
 
     return True
 
 
-def make_orn_types():
-    orn_types = defaultdict(lambda: defaultdict(list))
-    for n in range(1, 7):
-        for comb in itertools.product(range(-2, 3), repeat=n):
+def make_ornament_types(
+        max_length=6,
+        max_steps_below=2,
+        max_steps_above=2,
+        biggest_interval=3):
+    ornament_types = defaultdict(lambda: defaultdict(list))
+    for n in range(1, max_length + 1):
+        steps = range(-max_steps_below, max_steps_above + 1)
+        for combination in itertools.product(steps, repeat=n):
 
-            if not is_good(comb):
+            if not is_good(combination, biggest_interval=biggest_interval):
                 continue
 
-            sum_comb = sum(comb)
+            sum_comb = sum(combination)
             if sum_comb <= 0:
                 direction = 'ascending'
-                orn_types[n][direction].append(comb)
+                ornament_types[n][direction].append(combination)
 
             if sum_comb >= 0:
                 direction = 'descending'
-                orn_types[n][direction].append(comb)
-    return orn_types
+                ornament_types[n][direction].append(combination)
+    return ornament_types
 
 
-orn_types = make_orn_types()
+ornament_types = make_ornament_types()
 
 
 def choose(n, direction):
-    return random.choice(orn_types[n][direction])
+    return random.choice(ornament_types[n][direction])
