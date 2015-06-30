ec3efbc99a3d8246466e15115f2f7735083f10f9
Jonathan Marmor <jmarmor@gmail.com>
2015-06-29 23:09:36 -0400

    Flatten notation methods a little.

--- shortstories/shortstories.py
+++ shortstories/shortstories.py
@@ -164,18 +164,7 @@ class Piece(object):
                 part.append(measure)
             return
 
-        self.duet_options = [
-            ('ob', 'sax'),
-            ('fl', 'sax'),
-            ('cl', 'sax'),
-            ('ob', 'tpt'),
-            ('sax', 'tpt'),
-            ('cl', 'tpt'),
-            ('fl', 'tpt'),
-            ('fl', 'ob'),
-            ('fl', 'cl'),
-            ('ob', 'cl'),
-        ]
+        self.duet_options = None
 
         # 8 to 12 minutes
         max_duration = 12
@@ -210,59 +199,67 @@ class Piece(object):
         for song in self.songs:
             for bar in song.bars:
                 for part in bar.parts:
-                    measure = Measure()
-                    if bar.tempo:
-                        mark = MetronomeMark(
-                            number=bar.tempo,
-                            referent=Duration(1)
-                        )
-                        measure.insert(0, mark)
-                        measure.leftBarline = 'double'
-                    if bar.duration != previous_duration:
-                        ts = TimeSignature('{}/4'.format(bar.duration))
-                        measure.timeSignature = ts
-
-                    # Fix Durations
-                    durations = [note['duration'] for note in part['notes']]
-
-                    components_list = split_at_beats(durations)
-                    components_list = [join_quarters(note_components) for note_components in components_list]
-                    for note, components in zip(part['notes'], components_list):
-                        note['durations'] = components
-
-                    for note in part['notes']:
-                        if note['pitch'] == 'rest':
-                            n = Rest()
-                        else:
-                            if isinstance(note['pitch'], list):
-                                pitches = []
-                                for pitch_number in note['pitch']:
-                                    p = Pitch(pitch_number)
-                                    # Force all flats
-                                    if p.accidental.name == 'sharp':
-                                        p = p.getEnharmonic()
-                                    pitches.append(p)
-                                n = Chord(notes=pitches)
-
-                            else:
-                                p = Pitch(note['pitch'])
-                                # Force all flats
-                                if p.accidental.name == 'sharp':
-                                    p = p.getEnharmonic()
-                                n = Note(p)
-
-                        d = Duration()
-                        if note['duration'] == 0:
-                            d.quarterLength = .125
-                            d = d.getGraceDuration()
-                        else:
-                            d.fill(note['durations'])
-                        n.duration = d
-
-                        measure.append(n)
-
+                    measure = self.notate_measure(previous_duration, bar, part)
                     self.parts.d[part['instrument_name']].append(measure)
                 previous_duration = bar.duration
+
+    def notate_measure(self, previous_duration, bar, part):
+        measure = Measure()
+        if bar.tempo:
+            mark = MetronomeMark(
+                number=bar.tempo,
+                referent=Duration(1)
+            )
+            measure.insert(0, mark)
+            measure.leftBarline = 'double'
+        if bar.duration != previous_duration:
+            ts = TimeSignature('{}/4'.format(bar.duration))
+            measure.timeSignature = ts
+
+        # Fix Durations
+        durations = [note['duration'] for note in part['notes']]
+
+        components_list = split_at_beats(durations)
+        components_list = [join_quarters(note_components) for note_components in components_list]
+        for note, components in zip(part['notes'], components_list):
+            note['durations'] = components
+
+        # Notate
+        for note in part['notes']:
+            n = self.notate_note(note)
+            measure.append(n)
+
+        return measure
+
+    def notate_note(self, note):
+        if note['pitch'] == 'rest':
+            n = Rest()
+        else:
+            if isinstance(note['pitch'], list):
+                pitches = []
+                for pitch_number in note['pitch']:
+                    p = Pitch(pitch_number)
+                    # Force all flats
+                    if p.accidental.name == 'sharp':
+                        p = p.getEnharmonic()
+                    pitches.append(p)
+                n = Chord(notes=pitches)
+
+            else:
+                p = Pitch(note['pitch'])
+                # Force all flats
+                if p.accidental.name == 'sharp':
+                    p = p.getEnharmonic()
+                n = Note(p)
+
+        d = Duration()
+        if note['duration'] == 0:
+            d.quarterLength = .125
+            d = d.getGraceDuration()
+        else:
+            d.fill(note['durations'])
+        n.duration = d
+        return n
 
 
 if __name__ == '__main__':
